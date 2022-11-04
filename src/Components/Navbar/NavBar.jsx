2d820e4a49c1583723447d5c4ae41ed60c355294
lubermudezz <bermudezluciana@hotmail.com>
2022-11-04 16:17:29 -0300

    responsive navbar

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -7,7 +7,7 @@ import '../Navbar/NavBar.css'
 const NavBar = () => {
     return (
         <div className='navContainer'>
-            <span>
+            <span id='spanNavDesk'>
                 <Wrap>
                     <WrapItem>
                         <a href='#home'><Avatar name='Lu Bermudez' src='https://res.cloudinary.com/luubermudezz/image/upload/v1663225191/Portafolio/perfil_lkxqrt.jpg' /></a>
@@ -15,7 +15,7 @@ const NavBar = () => {
                 </Wrap>
                 <Text fontSize='2xl' className='text'><a href='#home'>Luciana Bermudez</a></Text>
             </span>
-            <ul className='ulNav'>
+            <ul id='ulNavDesk' className='ulNav'>
                     <a href='#projects'><li>Mis proyectos</li></a>
                     <a href='#skills'><li>Mis habilidades</li></a>
                     <a href='#about'><li>Sobre mí</li></a>
@@ -23,7 +23,22 @@ const NavBar = () => {
                     <a target='_blank' href='https://drive.google.com/file/d/1JJCO7MYCYZTu-pC0-7mQebwfCRnDHTxW/view?usp=sharing'><li>CV</li></a>
 
             </ul>
+            <ul className="ulNav" id='ulNavLaptop'>
+            <a href='#projects'><li>Proyectos</li></a>
+                    <a href='#skills'><li>Habilidades</li></a>
+                    <a href='#about'><li>Sobre mí</li></a>
+                    <a href='#contact'><li>Contacto</li></a>
+                    <a target='_blank' href='https://drive.google.com/file/d/1JJCO7MYCYZTu-pC0-7mQebwfCRnDHTxW/view?usp=sharing'><li>CV</li></a>
+            </ul>
+            <ul className='ulNav'>
+            <a href='#projects'><li>📂</li></a>
+                    <a href='#skills'><li>💻</li></a>
+                    <a href='#about'><li>🙋‍♀️</li></a>
+                    <a href='#contact'><li>📩</li></a>
+                    <a target='_blank' href='https://drive.google.com/file/d/1JJCO7MYCYZTu-pC0-7mQebwfCRnDHTxW/view?usp=sharing'><li>CV</li></a>
+            </ul>
         </div>
+        
     )
 }
 
