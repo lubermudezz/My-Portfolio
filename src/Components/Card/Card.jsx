79fc6f18717e5fb4ac52d2bb9d28380e2a2aa2a8
lubermudezz <bermudezluciana@hotmail.com>
2022-11-10 15:20:48 -0300

    new project

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Button, Stack} from '@chakra-ui/react'
+import { Button, Stack } from '@chakra-ui/react'
 import '../Card/Card.css'
 
 
-const Card = ({ title, text, linkDeploy, linkGithub, tech, video }) => {
+const Card = ({ title, text, linkDeploy, linkGithub, tech, video, id }) => {
     return (
         <div className='cardContainer'>
-            <div className='card' data-aos="fade-up" data-aos-duration="3000">
+            <div id={id} className='card' data-aos="fade-up" data-aos-duration="3000">
                 <video autoPlay muted loop>
-                <source src={video} type="video/mp4" />
+                    <source src={video} type="video/mp4" />
                 </video>
                 <div className='container'>
                     <h3>{title}</h3>
@@ -17,11 +17,13 @@ const Card = ({ title, text, linkDeploy, linkGithub, tech, video }) => {
                 <p className='textTech'>{tech}</p>
                 <div className='buttonsContainer'>
                     <Stack spacing={2} direction='row' align='center'>
-                        <a  href={linkDeploy} target='_blank'><Button id='mouseChange' className='buttonCardWidth' colorScheme='gray' size='md'>
-                            <img className='logoButtonCard' src="https://res.cloudinary.com/luubermudezz/image/upload/v1667269996/Portafolio/web_vgnblv.png" alt="lubermudez on linkedIn" width='25px' />
-                            Deploy
-                        </Button></a>
-                        <a  href={linkGithub} target='_blank' ><Button id='mouseChange' className='buttonCardWidth' colorScheme='gray' size='md'>
+                        {linkDeploy ? <a href={linkDeploy} target='_blank'>
+                            <Button id='mouseChange' className='buttonCardWidth' colorScheme='gray' size='md'>
+                                <img className='logoButtonCard' src="https://res.cloudinary.com/luubermudezz/image/upload/v1667269996/Portafolio/web_vgnblv.png" alt="lubermudez on linkedIn" width='25px' />
+                                Deploy
+                            </Button></a>
+                            : null}
+                        <a href={linkGithub} target='_blank' ><Button id='mouseChange' className='buttonCardWidth' colorScheme='gray' size='md'>
                             <img className='logoButtonCard' src="https://res.cloudinary.com/luubermudezz/image/upload/v1667269998/Portafolio/github_gutpsu.png" alt="lubermudez on github" width='25px' />
                             Github
                         </Button></a>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Projects/Project.css'
 import restoVid from '../../img/restoVid.mp4'
 import quizVid from '../../img/quizVid.mp4'
 import dogVid from '../../img/dogVid.mp4'
+import gamesVid from '../../img/gamesVid.mp4'
 import Card from '../Card/Card'
+import { Button, Stack } from '@chakra-ui/react'
+
 
 const Projects = () => {
+
+    const [display, setDisplay] = useState(false)
+
+    function handleClick(e) {
+       document.getElementById('moreInfo').style.display = 'block'
+       setDisplay(true)
+    }
+
+    function handleHideClick(e) {
+        document.getElementById('moreInfo').style.display = 'none'
+        setDisplay(false)
+    }
+
     return (
         <div className='projectDiv'>
             <div className='projectContainer'>
-                <Card title="Henry's Resto Project" tech='TypeScript, React | Redux, Node.js, MongoDB, Mongoose, Stripe, Tailwind, Cliengo, Google API' text='E-Commerce de comidas, proyecto grupal en el que implementamos autenticación local y de terceros, envío de notificaciones, chatbot, sistema de localización para entrega de pedidos, carrito y pasarela de pagos'  linkDeploy={'https://restohenry.vercel.app/'} linkGithub='https://github.com/lautaro012/E-Resto-Frontend-' video={restoVid} />
+                <Card title="Henry's Resto Project" tech='TypeScript, React | Redux, Node.js, MongoDB, Mongoose, Stripe, Tailwind, Cliengo, Google API, Nodemailer ' text='E-Commerce de comidas, proyecto grupal en el que implementamos autenticación local y de terceros, envío de notificaciones, chatbot, sistema de localización para entrega de pedidos, carrito y pasarela de pagos' linkDeploy={'https://restohenry.vercel.app/'} linkGithub='https://github.com/lautaro012/E-Resto-Frontend-' video={restoVid} />
+                <Card title="Game's Store" tech='JavaScript, React | Redux, PostgreSql, Express, Node.js, Stripe, Passport, Nodemailer' text='E-Commerce de videojuegos. Proyecto grupal, en el que el usuario puede obtener información de los juegos, agregarlos a favoritos y comprarlos para tenerlos en su lista. API: "Rawg"' linkGithub={'https://github.com/lubermudezz/HenryGamesStoreProject'} video={gamesVid} />
                 <Card title="Dog's App" tech='JavaScript, React | Redux, PostgreSql, Express, Node.js' text='Este es el primer proyecto que desarrollé. SPA en la que el usuario puede obtener información de las distintas razas de perro, crear nuevos, filtrar y ordenar según los distintos parámetros. API: "The Dog API"' linkGithub={'https://github.com/lubermudezz/Dog-s-App'} linkDeploy='https://dog-s-app.vercel.app/' video={dogVid} />
-                <Card title="Quiz Game" tech='JavaScript, Css, Html5' text='Demo de juego de preguntas, donde el usuario puede seleccionar respuestas y obtener una puntuación final en base a las respuestas correctas e incorrectas' linkDeploy={'https://quizgamelubermudez.netlify.app/'} linkGithub={'https://github.com/lubermudezz/Quiz-Game'} video={quizVid} />
+                <Card id='moreInfo' title="Quiz Game" tech='JavaScript, Css, Html5' text='Demo de juego de preguntas, donde el usuario puede seleccionar respuestas y obtener una puntuación final en base a las respuestas correctas e incorrectas' linkDeploy={'https://quizgamelubermudez.netlify.app/'} linkGithub={'https://github.com/lubermudezz/Quiz-Game'} video={quizVid} />
+                
             </div>
+            {display ? <Button id='buttonHideShow' onClick={(e) => handleHideClick(e)} >Ocultar</Button> : <Button id='buttonHideShow' onClick={(e) => handleClick(e)}>Ver más</Button>}
         </div>
     )
 }
